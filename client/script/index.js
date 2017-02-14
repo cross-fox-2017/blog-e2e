f26d82611850271ed151d79bd88576dc9a1fbf08
yoma sofwan <email": null>
2017-02-14 18:10:36 +0700

    perbaikan delete

--- client/script/index.js
+++ client/script/index.js
@@ -8,7 +8,7 @@ function ready () {
       for (var i = 0; i < booklist.length; i++) {
         let item = booklist[i]
         table += `<div class="row">
-         <div class="col s12 m6">
+         <div class="col s6 m6">
            <div class="card blue-grey darken-1">
              <div class="card-content white-text">
                <span class="card-title">${item.judul}</span>
@@ -52,7 +52,7 @@ function ready () {
           </div>
         </div>
 
-        
+
 
         `
       }
