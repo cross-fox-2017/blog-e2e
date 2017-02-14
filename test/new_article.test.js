1140d0c1a68e28cc0eca787152ab7d78f0ba41f2
timocodex <timothy.og.siahaan@gmail.com>
2017-02-14 14:53:42 +0700

    done release 0

---
+++ test/new_article.test.js
@@ -0,0 +1,26 @@
+const Nightmare = require('nightmare')
+const should = require('chai').should()
+
+describe('test new article',function(){
+  this.timeout(50000)
+  it('should return the right title of the article',function(done){
+    var nightmare = Nightmare({show: true})
+    nightmare
+    .goto('http://127.0.0.1:8080/article.html')
+    .wait('[name=title]')
+    .click('[name=title]')
+    .type('[name=title]','ayam bawang')
+    .click('[name=story]')
+    .type('[name=story]','masukkan ayamnya goreng dengan bawangnya...nah hidupin apinya tunggu 1 jam jadilah ayam bawang')
+    .click('[name=newpost]')
+    .wait('#idpost')
+    .evaluate(function(){
+      return document.querySelector('#idpost').innerHTML
+    })
+    .end()
+    .then(function(result){
+      result.should.be.equal('ayam bawang');
+      done()
+    })
+  })
+})
