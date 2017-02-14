28fd430220bf0afe9703b67708693142944fcbed
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-14 18:38:14 +0700

    update nightmare

--- server/test/nightmare.test.blog.js
+++ server/test/nightmare.test.blog.js
@@ -1,24 +1,51 @@
-// const mongoose = require('mongoose')
-// const Blog = require('../models/model.blogs')
-// let chai = require('chai')
-// let chaiHttp = require('chai-http')
-// chai.use(chaiHttp)
-// let should = chai.should()
-var Nightmare = require('nightmare')
-var nightmare = Nightmare({ show: true })
+const Nightmare = require('nightmare')
+const should = require('chai').should()
 
-nightmare
-  .goto('https://duckduckgo.com')
-  .type('#search_form_input_homepage', 'github nightmare')
-  .click('#search_button_homepage')
-  .wait('#zero_click_wrapper .c-info__title a')
-  .evaluate(function () {
-    return document.querySelector('#zero_click_wrapper .c-info__title a').href
+describe('create new blog', function (done) {
+  this.timeout(100000)
+  it('should return title of the blog', function (done) {
+    var nightmare = Nightmare({show: true})
+    nightmare
+      .goto('http://127.0.0.1:8080/blog.html')
+      .click('nav a.waves-effect')
+      .click('#create-blog-title')
+      .type('#create-blog-title', 'Batu Super')
+      .click('#create-author-name')
+      .type('#create-author-name', 'Syanmil')
+      .click('#create-text-blog')
+      .type('#create-text-blog', 'Batu bukan hanya sebagai acc biasa, batu memiliki energi..')
+      .click('#modal1 a')
+      .evaluate(function () {
+        // return document.querySelector('#data5 td.author').innerHTML
+        return document.querySelector('#blog-list div div div span').innerHTML
+      })
+      .end()
+      .then(function (result) {
+        result.should.to.equal('Batu Super')
+        done()
+      }).catch(function (err) {
+      console.log(err)
+    })
   })
-  .end()
-  .then(function (result) {
-    console.log(result)
+})
+
+describe('delete a blog', function (done) {
+  this.timeout(100000)
+  it('blog will not found', function (done) {
+    var nightmare = Nightmare({show: true})
+    nightmare
+      .goto('http://127.0.0.1:8080/blog.html')
+      .click('#blog-list .delete-blog')
+      .evaluate(function () {
+        // return document.querySelector('#data5 td.author').innerHTML
+        return document.querySelector('#blog-list div div div span').innerHTML
+      })
+      .end()
+      .then(function (result) {
+        result.should.to.equal(null)
+        done()
+      }).catch(function (err) {
+      console.log(err)
+    })
   })
-  .catch(function (error) {
-    console.error('Search failed:', error)
-  })
+})
