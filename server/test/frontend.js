fec5eb12545a3209fd26eaf55e1907f585280225
YOMA <email": null>
2017-02-18 11:49:48 +0700

    selesai tes delete

--- server/test/frontend.js
+++ server/test/frontend.js
@@ -7,40 +7,18 @@ const Nightmare = require('Nightmare');
 
 describe('menjalankan method untuk artikel', function() {
   this.timeout(10000)
-  // it('artikel berhasil disimpan', function(done){
-  //   var nightmare = Nightmare({show: true})
-  //   nightmare
-  //   .goto('http://localhost:8080')
-  //   .wait('#judul')
-  //   .click('#judul')
-  //   .type('#judul','ini judul')
-  //   .click('#isi')
-  //   .type('#isi','ini isi')
-  //   .click('#penulis')
-  //   .type('#penulis','yomaswn')
-  //   .click('button[type=submit]')
-  //   .wait(1000)
-  //   .evaluate(function(){
-  //     return document.querySelector('.card-title').innerHTML
-  //   })
-  //   .end()
-  //   .then(function(result) {
-  //     // result.should.be.equal('ini judul')
-  //     result.should.be.a('string')
-  //     done()
-  //   })
-  // })
-
-  it('artikel berhasil diubah', function(done){
+  it('artikel berhasil disimpan', function(done){
     var nightmare = Nightmare({show: true})
     nightmare
     .goto('http://localhost:8080')
-    .wait('.card-title')
-    .click('.update')
-    .wait('.modal')
-    .click('.modal #judul')
-    .type('.modal #judul', 'edit lagi sepanjang ini')
-    .click('.modal button[type=submit]')
+    .wait('#judul')
+    .click('#judul')
+    .type('#judul','ini judul')
+    .click('#isi')
+    .type('#isi','ini isi')
+    .click('#penulis')
+    .type('#penulis','yomaswn')
+    .click('button[type=submit]')
     .wait(1000)
     .evaluate(function(){
       return document.querySelector('.card-title').innerHTML
@@ -53,4 +31,44 @@ describe('menjalankan method untuk artikel', function() {
     })
   })
 
+  it('artikel berhasil diubah', function(done){
+    const nightmare = Nightmare({show: true});
+    nightmare
+    .goto('http://localhost:8080')
+    .wait('.card-title')
+    .click('a[name=modaledit]')
+    .wait('.modal')
+    .click('.modal input[type=text]')
+    .type('.modal input[type=text]', 'edit')
+    .click('.modal .materialize-textarea')
+    .type('.modal .materialize-textarea', 'edit')
+    .click('.modal .modal-action')
+    .evaluate(function(){
+      return document.querySelector('.card-title').innerHTML
+    })
+    .end()
+    .then((result) => {
+      result.should.be.a('string')
+      done()
+    })
+  })
+
+
+  it('artikel berhasil dihapus', function(done){
+    const nightmare = Nightmare({show: true});
+    nightmare
+    .goto('http://localhost:8080')
+    .wait('.card-title')
+    .click('a[name=delete]')
+    .evaluate(function(){
+      if(document.querySelector('.card-title').innerHTML) {
+        return "abc"
+      }
+    })
+    .end()
+    .then((result) => {
+      result.should.equal("abc")
+      done()
+    })
+  })
 })
